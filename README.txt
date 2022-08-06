0afef3d0c7f08bc3127a488309de56ab26f8a394
TyrusYeo <tyrusyeo5@gmail.com>
2022-08-06 23:27:32 +0800

    New files for new project

---
+++ README.txt
@@ -0,0 +1,5 @@
+Progress on my journey in learning API use (through Genius API) and Python so that I can develop a Lyric
+generator based on an artist the user typed in. 
+e.g Drake inspired rap song. 
+
+ 
---
+++ main.py
@@ -0,0 +1,22 @@
+import lyricsgenius as lg 
+
+file = open(r"C:\Users\Tyrus Yeo\Desktop\Coding\Lyric generator\lyrics.txt", "w")
+
+genius = lg.Genius('-lgbBbLxKVAdN311MF18o5fyAUZbKi0O2C6xeXbdehUvGMc8ET6nmVkUD3XJpxpw', 
+skip_non_songs=True, excluded_terms=["(Remix)", "(Live)"], remove_section_headers=True)
+
+def getLyrics(arr, m):
+    c = 0
+    for name in arr:
+        try: 
+            songs = (genius.search_artist(name, max_songs = m, sort = 'popularity')).songs
+            s = [song.lyrics for song in songs]
+            file.write("\n \n   <|endoftext|>   \n \n".join(s))
+            c += 1
+            print(f"Songs grabbed:{len(s)}")
+        except: 
+            print(f"Some exception at {name}: {c}")
+
+artists = ["John Mayer"]
+
+getLyrics(artists, 3)
